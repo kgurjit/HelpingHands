7515a4953e1e2822b8a471ba041b4cc6259ead53
David Li <jdavidli01@outlook.com>
2016-08-15 18:37:29 -0400

    create function, orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,15 @@
 var connection = require('../config/connection.js');
 
+function printQuestionMarks(num) {
+	var arr = [];
+
+	for (var i = 0; i < num; i++) {
+		arr.push('?');
+	}
+
+	return arr.toString();
+}
+
 var orm = {
 	allAds: function (tableName, cb) {
 		var queryString = 'SELECT * FROM ' + tableName + ';';
@@ -7,5 +17,24 @@ var orm = {
 			if (err) throw err;
 			cb(result);
 		});
+	},
+	create: function () {
+		var queryString = 'INSERT INTO ' + table;
+
+		queryString = queryString + ' (';
+		queryString = queryString + cols.toString();
+		queryString = queryString + ') ';
+		queryString = queryString + 'VALUES (';
+		queryString = queryString + printQuestionMarks(vals.length);
+		queryString = queryString + ') ';
+
+		console.log(queryString);
+
+		connection.query(queryString, vals, function (err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+
 	}
+
 };
